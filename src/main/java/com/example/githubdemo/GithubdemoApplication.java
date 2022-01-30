b92be25b56acfef33d784ef4e1dc3f48231d0860
Eliud Rojas <erojas@fenixit.mx>
2022-01-29 18:45:17 -0600

    Commets added

--- src/main/java/com/example/githubdemo/GithubdemoApplication.java
+++ src/main/java/com/example/githubdemo/GithubdemoApplication.java
@@ -3,6 +3,9 @@ package com.example.githubdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Agragando comentarios
+ */
 @SpringBootApplication
 public class GithubdemoApplication {
 
